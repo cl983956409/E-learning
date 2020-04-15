670d4ca7fcaec99246cf2b0f28fe82c248d1b5aa
cl983956409 <983956409@qq.com>
2020-04-15 11:18:45 +0800

    Mod 初始化项目

---
+++ src/main/java/com/learning/electronic/demo/ELearningApplication.java
@@ -0,0 +1,13 @@
+package com.learning.electronic.demo;
+
+/**
+ * @author 程龙[chenglonghy]
+ * @date 2020/4/15 - 10:41
+ * @history 2020/4/15 - 10:41 chenglonghy  create.
+ */
+@SpringBootApplication
+public class ELearningApplication {
+    public static void main(String[]args){
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
